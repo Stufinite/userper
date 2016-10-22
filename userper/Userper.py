da216a7a75c27868b6e8289b87befbdcaf6b15e7
CJHwong <cjhwong@gmail.com>
2016-10-22 13:17:44 +0800

    Added variables for django model

--- userper/Userper.py
+++ userper/Userper.py
@@ -7,13 +7,40 @@ class Userper():
     def __init__(self, location):
         self.location = location
 
-    def get_user(self, session_id=''):
+    def get(self, session_id=''):
+        user = self._get_user(session_id)
+        if user != None:
+            self.username = user['username']
+            self.email = user['email']
+            self.last_name = user['last_name']
+            self.first_name = user['first_name']
+            self.grade = user['grade']
+            self.major = user['major']
+
+    def get_test(self, session_id=''):
+        user = {
+            'username': 'root',
+            'email': 'root@mail.stufinite.faith',
+            'last_name': 'ot',
+            'first_name': 'ro',
+            'grade': 1,
+            'major': 'CS'
+        }
+        if user != None:
+            self.username = user['username']
+            self.email = user['email']
+            self.last_name = user['last_name']
+            self.first_name = user['first_name']
+            self.grade = user['grade']
+            self.major = user['major']
+
+    def _get_user(self, session_id=''):
         r = urllib.request.urlopen(
             'http://' + self.location + '/auth/get_user/' + session_id)
         text = bytes.decode(r.readline())
         return text if text == 'None' else json.loads(text)
 
-    def get_username(self, session_id=''):
+    def _get_username(self, session_id=''):
         r = urllib.request.urlopen(
             'http://' + self.location + '/auth/get_username/' + session_id)
         return bytes.decode(r.readline())
