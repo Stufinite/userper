c83e5f9bb3c763c4fed6ac4ca91163c96d0992eb
CJHwong <cjhwong@gmail.com>
2016-12-08 18:34:24 +0800

    Added http header to request

--- userper/Userper.py
+++ userper/Userper.py
@@ -38,12 +38,14 @@ class Userper():
             self.career = user['career']
 
     def _get_user(self, session_id=''):
-        r = urllib.request.urlopen(
-            'http://' + self.location + '/auth/get_user/' + session_id)
+        req = urllib.request.Request('http://' + self.location + '/auth/get_user/' + session_id,
+                                     headers={'User-Agent': "Magic Browser"}, headers={'User-Agent': "Magic Browser"})
+        r = urllib.request.urlopen(req)
         text = bytes.decode(r.readline())
         return text if text == 'None' else json.loads(text)
 
     def _get_username(self, session_id=''):
-        r = urllib.request.urlopen(
-            'http://' + self.location + '/auth/get_username/' + session_id)
+        req = urllib.request.Request('http://' + self.location + '/auth/get_username/' + session_id,
+                                     headers={'User-Agent': "Magic Browser"}, headers={'User-Agent': "Magic Browser"})
+        r = urllib.request.urlopen(req)
         return bytes.decode(r.readline())
